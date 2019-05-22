53844ca1bd51ccdaf941aac86179efb9ad3ee5dc
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-22 15:32:28 -0600

    fixes hogwarts service as per new spec

--- app/services/hogwarts_service.rb
+++ app/services/hogwarts_service.rb
@@ -3,9 +3,33 @@ class HogwartsService
     @house = house
   end
 
-  def get_students
-    conn = Faraday.new("http://hogwarts-it.herokuapp.com/api/v1/house/#{@house}?api_key=#{ENV["HOGWARTS_KEY"]}")
-    response_body = conn.get.body
-    students = JSON.parse(response_body, symbolize_names: true)[:data][0][:attributes]
+  def house_converter(house)
+    if @house == "Slytherin"
+     4
+   elsif @house == "Griffyndor"
+     1
+   elsif @house == "Ravenclaw"
+     2
+   elsif @house == "Hufflepuff"
+     3
+    end
   end
+
+  def get_students
+    response_body = conn.get.body
+    students = JSON.parse(response_body, symbolize_names: true)
+    # binding.pry
+  end
+
+  private
+    def conn
+      @_conn ||= faraday_block
+    end
+
+    def faraday_block
+      Faraday.new("https://hogwarts-as-a-service.herokuapp.com/api/v1/house/#{house_converter(@house)}") do |f|
+        f.headers['x_api_key'] = ENV["HOGWARTS_KEY"]
+        f.adapter Faraday.default_adapter
+      end
+    end
 end
--- spec/services/hogwarts_service_spec.rb
+++ spec/services/hogwarts_service_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe HogwartsService do
   context "instance methods" do
     it "gets students" do
-      house = "slytherin"
+      house = "Slytherin"
       service = HogwartsService.new(house)
 
       return_value = service.get_students
 
-      expect(return_value).to be_a(Hash)
-      expect(return_value[:name]).to eq(house.capitalize)
+      expect(return_value).to be_a(Array)
+      expect(return_value[0]).to be_a(Hash)
     end
   end
 end
